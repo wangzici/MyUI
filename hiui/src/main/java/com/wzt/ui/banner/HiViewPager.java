69297c79bc75d37cae9eaf50f5dc897735124058
王子韬 <wangzitao@xiaomalixing.com>
2020-07-18 00:24:27 +0800

    feature:fix:针对ViewPager在RecycleView中的典型问题进行修复

--- hiui/src/main/java/com/wzt/ui/banner/HiViewPager.java
+++ hiui/src/main/java/com/wzt/ui/banner/HiViewPager.java
@@ -1,5 +1,6 @@
 package com.wzt.ui.banner;
 
+import android.app.Activity;
 import android.content.Context;
 import android.os.Handler;
 import android.util.AttributeSet;
@@ -8,6 +9,8 @@ import android.view.MotionEvent;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.viewpager.widget.ViewPager;
+
+import java.lang.reflect.Field;
 
 /**
  * Created by Kyrie
@@ -24,13 +27,13 @@ public class HiViewPager extends ViewPager {
      * 是否开启自动轮播
      */
     private boolean mAutoPlay = true;
+    private boolean isLayout;
     private Handler mHandler = new Handler();
     private Runnable mRunnable = new Runnable() {
         @Override
         public void run() {
             //切换到下一个
-            int nextItem = next();
-            setCurrentItem(nextItem);
+            next();
             mHandler.postDelayed(this, mIntervalTime);
         }
     };
@@ -56,7 +59,6 @@ public class HiViewPager extends ViewPager {
 
     public void stop() {
         mHandler.removeCallbacksAndMessages(null);
-        mAutoPlay = false;
     }
 
     private void start() {
@@ -97,5 +99,37 @@ public class HiViewPager extends ViewPager {
                 break;
         }
         return super.onTouchEvent(ev);
+    }
+
+    @Override
+    protected void onLayout(boolean changed, int l, int t, int r, int b) {
+        super.onLayout(changed, l, t, r, b);
+        isLayout = true;
+    }
+
+    @Override
+    protected void onDetachedFromWindow() {
+        if (((Activity) getContext()).isFinishing()) {
+            //ViewPager典型问题2：若从RecycleView等控件被回收后，正在播放的动画会被中断
+            //解决办法：只在Activity真的被finishing时才去调用
+            super.onDetachedFromWindow();
+        }
+        stop();
+    }
+
+    @Override
+    protected void onAttachedToWindow() {
+        super.onAttachedToWindow();
+        //ViewPager典型问题1：若从RecycleView等控件被回收后，重新绑定第一次滚动会无动画
+        if (isLayout && getAdapter() != null && getAdapter().getCount() > 0) {
+            try {
+                Field mFirstLayout = ViewPager.class.getDeclaredField("mFirstLayout");
+                mFirstLayout.setAccessible(true);
+                mFirstLayout.set(this, false);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        start();
     }
 }
